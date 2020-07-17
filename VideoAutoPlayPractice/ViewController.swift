792fad310959b5278175e4d2320dcc2c4f11c160
pratikgajbhiye222 <pratik.gajbhiye222@gmail.com>
2020-07-17 14:00:26 +0530

    Initial Commit

---
+++ VideoAutoPlayPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoAutoPlayPractice
+//
+//  Created by pratik gajbhiye on 17/07/20.
+//  Copyright © 2020 Mobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
